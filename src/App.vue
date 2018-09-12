344acbb7a7515e7a540c8bf8e7110ef758199b1d
Francis Cote <drfeelgoud@gmail.com>
2018-09-12 10:13:27 -0400

    Added Vue Conf callout + some UI tweaks

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <p class="vueconf-callout">We're very excited to sponsor <a href="https://vuetoronto.com">VueConf TO 2018</a>. Come hang out and learn some Vue.js with world-class developers (Nov. 15-16).</p>
     <h1>
       Welcome!
     </h1>
@@ -13,33 +14,53 @@
     <p>
       <i>Comments aren't saved anywhere and will be flushed upon reload</i>
     </p>
-    <p>
-      Made with {code} by <a href="https://snipcart.com">Snipcart</a>
-      | <a href="https://twitter.com/snipcart" >Twitter</a>
-      | <a href="https://github.com/snipcart">GitHub</a>
-      | <a href="https://facebook.com/snipcart">Facebook</a>
-    </p>
+    <div class="footer">
+      <p>
+        Made with {code} by <a href="https://snipcart.com">Snipcart</a>
+      </p>
+      <p>
+        <a href="https://twitter.com/snipcart" >Twitter</a>
+        | <a href="https://github.com/snipcart">GitHub</a>
+        | <a href="https://facebook.com/snipcart">Facebook</a>
+      </p>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
-body{
-  background-color: #56585a
+$yellow: #f6e770;
+$gray: #56585a;
+$dark: #2c2e31;
+
+body {
+  background-color: $gray;
 }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color:#fff;
-  margin-top: 60px;  
+  color: white;
+  margin-top: 60px;
 
-  a, a:visited{
-    color:#f6e770;
+  a,
+  a:visited {
+    color: $yellow;
   }
 
-  .loves{
-  font-size: 2rem; 
+  .vueconf-callout {
+    margin: auto;
+    display: inline-block;
+    padding: 1.5rem;
+    background-color: $dark;
+  }
+
+  .loves {
+    font-size: 3rem;
+  }
+
+  .footer{
+    margin-top: 3rem;
   }
 }
 </style>
